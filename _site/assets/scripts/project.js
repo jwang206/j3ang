789061fc41b0366ffe68eca3ba770c47295f9d05
jwang206 <jwang206@hawk.iit.edu>
2018-10-07 20:07:55 -0500

    update resize videos script

--- _site/assets/scripts/project.js
+++ _site/assets/scripts/project.js
@@ -29,35 +29,32 @@
 
 
 	// resize embeded videos
-	$(function () {
+	var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
+		$fluidEl = $("figure");
 
-		var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
-			$fluidEl = $("figure");
+	$allVideos.each(function () {
 
+		$(this)
+			// jQuery .data does not work on object/embed elements
+			.attr('data-aspectRatio', this.height / this.width)
+			.removeAttr('height')
+			.removeAttr('width');
+
+	});
+
+	$(window).resize(function () {
+
+		var newWidth = $fluidEl.width();
 		$allVideos.each(function () {
 
-			$(this)
-				// jQuery .data does not work on object/embed elements
-				.attr('data-aspectRatio', this.height / this.width)
-				.removeAttr('height')
-				.removeAttr('width');
+			var $el = $(this);
+			$el
+				.width(newWidth)
+				.height(newWidth * $el.attr('data-aspectRatio'));
 
 		});
 
-		$(window).resize(function () {
-
-			var newWidth = $fluidEl.width();
-			$allVideos.each(function () {
-
-				var $el = $(this);
-				$el
-					.width(newWidth)
-					.height(newWidth * $el.attr('data-aspectRatio'));
-
-			});
-
-		}).resize();
-
-	});
+	}).resize();
+	// resize embeded videos end
 
 }(jQuery));
