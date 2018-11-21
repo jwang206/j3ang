1a452914ddb557b1ef2b5e7a97ab3562a474e3bc
j3ang <j3ang.info@gmail.com>
2018-11-21 17:56:49 -0600

    add isotope js

--- _site/assets/scripts/home.js
+++ _site/assets/scripts/home.js
@@ -1,18 +1,35 @@
 var sr = ScrollReveal({
-	origin   : "bottom",
-	distance : "64px",
-	duration : 800,
-	delay    : 0,
-	scale    : 1
+    origin: "bottom",
+    distance: "64px",
+    duration: 800,
+    delay: 0,
+    scale: 1
 });
 
 sr.reveal('.projects-list a');
 sr.reveal('.posts-list a');
 
 var header = anime({
-  targets: '.header .overlay-div',
-  translateY: 15,
-	loop: true
+    targets: '.header .overlay-div',
+    translateY: 15,
+    loop: true
 });
 
 anime.speed = 0.1;
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+    // options
+});
+// filter items on button click
+$('.filter-button-group').on('click', 'a', function () {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({
+        filter: filterValue
+    });
+});
+$('.button-group a.button').on('click', function () {
+    $('.button-group a.button').removeClass('active');
+    $(this).addClass('active');
+});
