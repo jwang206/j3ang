7a249064afcd0cef446442072f6479a4c6e89e41
j3ang <j3ang.info@gmail.com>
2018-12-01 13:04:27 -0600

    added chatting app

--- _site/assets/scripts/home.js
+++ _site/assets/scripts/home.js
@@ -1,35 +1,58 @@
-var sr = ScrollReveal({
-    origin: "bottom",
-    distance: "64px",
-    duration: 800,
-    delay: 0,
-    scale: 1
-});
-
-sr.reveal('.projects-list a');
-sr.reveal('.posts-list a');
-
-var header = anime({
-    targets: '.header .overlay-div',
-    translateY: 15,
-    loop: true
-});
-
-anime.speed = 0.1;
+$(document).ready(function () {
 
 
-// init Isotope
-var $grid = $('.grid').isotope({
-    // options
-});
-// filter items on button click
-$('.filter-button-group').on('click', 'a', function () {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({
-        filter: filterValue
+    // set content margin bottom 
+    $('.content').css('margin-bottom', $('.footer').height() + 'px');
+    $(window).on('resize', function () {
+        // set content margin bottom 
+        $('.content').css('margin-bottom', $('.footer').height() + 'px');
+        $('.content').scrollTop(0, 0);
+
     });
 });
-$('.button-group a.button').on('click', function () {
-    $('.button-group a.button').removeClass('active');
-    $(this).addClass('active');
-});
+
+window.onload = function () {
+    console.log("called top");
+
+
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on('click', 'a', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+    $('.button-group a.button').on('click', function () {
+        $('.button-group a.button').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $(window).scrollTop(0, 0);
+
+};
+
+
+// var sr = ScrollReveal({
+//     origin: "bottom",
+//     distance: "64px",
+//     duration: 800,
+//     delay: 0,
+//     scale: 0
+// });
+
+// sr.reveal('.projects-list a');
+// sr.reveal('.posts-list a');
+
+// var header = anime({
+//     targets: '.header .overlay-div',
+//     translateY: 15,
+//     loop: true
+// });
+
+// anime.speed = 0.5;
+
+$(window).scrollTop(0, 0);
