d0dd2a0ddb8a1b82575b470c5b432f1bf11cad0c
j3ang <j3ang.info@gmail.com>
2019-02-21 10:58:09 -0600

    fixed article position onload

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -17,8 +17,7 @@
         return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
     }
 
-    $(function () {
-        $(window).on('load resize scroll', function () {
+    $(window).on('resize scroll', function () {
 
 
             sticky();
