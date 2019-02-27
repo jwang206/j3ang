78fb620c7cd2c17ceff719d2fd5cfa39b260c59b
j3ang <j3ang.info@gmail.com>
2019-02-26 18:34:40 -0600

    change stick-to-parent class change for tabcontrol

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -36,9 +36,10 @@ window.onload = function () {
     // sticky tab with animation
     $(window).scroll(function () {
         if ($(this).scrollTop() < 100) {
-            $('.tabcontrol').stick_in_parent({
+            $('.navbar').stick_in_parent({
                 offset_top: $('.header').height()
             });
+
         } else {
             $('.content').css({
                 top: $('.header').height() + "px"
