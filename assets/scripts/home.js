65d2e0ac21995e85392f156a257f02e052842c43
j3ang <j3ang.info@gmail.com>
2019-04-04 15:30:40 -0500

    isotope layout

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -25,8 +25,10 @@ window.onload = function () {
     // init Isotope
     var $grid = $('.grid').isotope({
         // options
+        percentPosition: true,
         layoutMode: 'fitRows'
     });
+
     // filter items on button click
     $('.filter-button-group').on('click', 'a', function () {
         var filterValue = $(this).attr('data-filter');
@@ -34,6 +36,7 @@ window.onload = function () {
             filter: filterValue
         });
     });
+
     $('.button-group a.button').on('click', function () {
         $('.button-group a.button').removeClass('active');
         $(this).addClass('active');
